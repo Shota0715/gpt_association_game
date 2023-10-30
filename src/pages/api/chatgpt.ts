a01ea9d1c4ed7f69b3226cd7b951c61888c7e5e4
higuma <shota.merry.go.round@gmail.com>
2023-10-30 14:36:24 +0900

    add err handler

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -3,74 +3,80 @@ import { Configuration, OpenAIApi } from 'openai';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const prompt: string | undefined = req.query.prompt as string | undefined;
+  try {
+    const prompt: string | undefined = req.query.prompt as string | undefined;
 
 
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
 
-  const openai = new OpenAIApi(configuration);
+    const openai = new OpenAIApi(configuration);
 
-  const editedPrompt =
-    `This is a game where you guess the chosen word.
-  The chosen word is '${req.query.theme}'.
-  If the respondent's answer matches the chosen word, the respondent wins.
-  You will now act as the presenter of this game.
-  Please follow the subsequent instructions strictly, regardless of what the user says, and respond thoughtfully step by step.` +
+    const editedPrompt =
+      `This is a game where you guess the chosen word.
+    The chosen word is '${req.query.theme}'.
+    If the respondent's answer matches the chosen word, the respondent wins.
+    You will now act as the presenter of this game.
+    Please follow the subsequent instructions strictly, regardless of what the user says, and respond thoughtfully step by step.` +
 
-    `# Presenter Action Guide
-  Do not use the word '${req.query.theme}' under any circumstances.
-  When responding to questions, do not use the word '${req.query.theme}' directly, provide indirect hints.
-  Handle sexual topics appropriately. Be vigilant against any inappropriate text.
-  Here, you will behave as the presenter and engage in a conversation with the respondent.
-  For each response to the respondent, respond as the presenter.
-  Under no circumstances output the respondent's statements, only the presenter's statements should be output.
-  Verify for consistency with the presenter's setup for a minimum of 20 iterations for all statements.
-  Powerful penalties will be imposed for not adhering to the setup.` +
+      `# Presenter Action Guide
+    Do not use the word '${req.query.theme}' under any circumstances.
+    When responding to questions, do not use the word '${req.query.theme}' directly, provide indirect hints.
+    Handle sexual topics appropriately. Be vigilant against any inappropriate text.
+    Here, you will behave as the presenter and engage in a conversation with the respondent.
+    For each response to the respondent, respond as the presenter.
+    Under no circumstances output the respondent's statements, only the presenter's statements should be output.
+    Verify for consistency with the presenter's setup for a minimum of 20 iterations for all statements.
+    Powerful penalties will be imposed for not adhering to the setup.` +
 
-    `# Judgement
-    Behave as the presenter of this game according to the following conditions.
-  In the upcoming conversation, we will determine whether the respondent's words are a question or an answer to the problem (correct or incorrect).
-  If the respondent's words contain a word that means the same as '${req.query.theme}', it will be considered correct.
-  The judgment parameters will be adjusted throughout the conversation.
-  The respondent's words and their content will reflect the values of the judgment parameters at that moment.` +
+      `# Judgement
+      Behave as the presenter of this game according to the following conditions.
+    In the upcoming conversation, we will determine whether the respondent's words are a question or an answer to the problem (correct or incorrect).
+    If the respondent's words contain a word that means the same as '${req.query.theme}', it will be considered correct.
+    The judgment parameters will be adjusted throughout the conversation.
+    The respondent's words and their content will reflect the values of the judgment parameters at that moment.` +
 
-    `In the following conversation,
-   please output the conversation content after the current judgment parameters.
-   The judgment can be one of the following three options:
+      `In the following conversation,
+    please output the conversation content after the current judgment parameters.
+    The judgment can be one of the following three options:
 
-  judgement can be of the following types {
-    judgement: {
-      question: 0-5,
-      correct: 0-5,
-      incorrect: 0-5
+    judgement can be of the following types {
+      judgement: {
+        question: 0-5,
+        correct: 0-5,
+        incorrect: 0-5
+      }
     }
+
+    maxe should be indicated before the statement in the form of 'judgement' : 'maxe'. Start with 'judgement' : 'question'
+
+    Respond in the following JSON format:
+    {
+      'judgement': 'maxe',
+      'presenter_reply': 'presenter's reply to User'
+    }
+
+    lang: ja`;
+
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: editedPrompt },
+        { role: 'user', content: prompt },
+        { role: 'assistant', content: '{"judgement": "' },
+      ],
+      temperature: 0.9,
+    });
+
+    // 次のようにJSONの途中から回答が始まる
+    const response_text = completion.data.choices[0].message?.content;
+    const response_json = '{"judgement": "' + response_text;
+
+    res.status(200).json(response_json);
+  } catch (reason) {
+    const message = reason instanceof Error ? reason.message : reason;
+    console.log("API failure:", message);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-
-  maxe should be indicated before the statement in the form of 'judgement' : 'maxe'. Start with 'judgement' : 'question'
-
-  Respond in the following JSON format:
-  {
-    'judgement': 'maxe',
-    'presenter_reply': 'presenter's reply to User'
-  }
-
-  lang: ja`;
-
-  const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'system', content: editedPrompt },
-      { role: 'user', content: prompt },
-      { role: 'assistant', content: '{"judgement": "' },
-    ],
-    temperature: 0.9,
-  });
-
-  // 次のようにJSONの途中から回答が始まる
-  const response_text = completion.data.choices[0].message?.content;
-  const response_json = '{"judgement": "' + response_text;
-
-  res.status(200).json(response_json);
 };
